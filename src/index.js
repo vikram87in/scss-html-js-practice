c0ed3fcc13a2c6b6bba1962ebb5a78ce204b4b0b
vikram87in <39241954+vikram87in@users.noreply.github.com>
2022-03-01 20:26:57 +0530

    another commit from online editor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-console
-console.log('hiiii');
+console.log('hiiii hello');
 // import { NAME as name } from './index2';
 // import * as mod from './indexImpExp';
 // console.log(name);
